85111422268c59f2ba81e2811ec93f2c4fb74754
kennedyufersa <73143709+kennedyufersa@users.noreply.github.com>
2022-05-11 10:54:37 -0300

    Update main.cpp

--- cidades/cidades/main.cpp
+++ cidades/cidades/main.cpp
@@ -1,5 +1,20 @@
+#include<stdlib.h>
 #include<stdio.h>
 #include<math.h>
+
+
+void rotas(){
+    FILE *f = fopen("tourar9152.tour", "r");
+    int a, b;
+    fscanf(f, "%d", &a);
+    fscanf(f, "%d", &b);
+    while (!feof(f)){
+        printf("Rotas %d %d\n", a, b);
+        a = b;
+        fscanf(f, "%d", &b);
+    }
+    fclose(f);
+}
 
 int main(){
     FILE *arquivo = fopen("ar9152.tsp", "r");
@@ -10,13 +25,13 @@ int main(){
     double cidLa[N];
     double cidLg[N];
 
-    double *dist;
-    dist = (double*)malloc(N*N*sizeof(double));
+    double *grafo;
+    grafo = (double*)malloc(N*N*sizeof(double));
     for (int i = 0; i < N; i++)
     {
         for (int j = 0; j < N; j++)
         {
-            dist[i*N + j] = 0.;
+            grafo[i*N + j] = 0.;
         }
     }
     
@@ -30,12 +45,10 @@ int main(){
         k++;
     }
     fclose(arquivo);
-
-    for (int i = 0; i < N; i++)
-    {
-        for (int j = i; j < N; j++)
-        {
-            dist[i*N + j] = sqrt(pow(cidLa[i] - cidLa[j], 2) + pow(cidLg[i] - cidLg[j], 2));
+    for (int i = 0; i < N; i++){
+        for (int j = i+1; j < N; j++){
+            grafo[i*N + j] = sqrt(pow(cidLa[i] - cidLa[j], 2) + pow(cidLg[i] - cidLg[j], 2));
         }
     }
+    rotas();
 }
